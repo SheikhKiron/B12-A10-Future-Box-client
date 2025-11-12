0b2b60bf9b7790e98c278c84b82a136251564475
SheikhKiron <sheikhkiron59@gmail.com>
2025-11-12 21:45:16 +0600

    Update some text

--- src/Components/Swiper/Slider.jsx
+++ src/Components/Swiper/Slider.jsx
@@ -30,7 +30,7 @@ export default function Slider() {
             src="https://plus.unsplash.com/premium_photo-1681140560805-de6115554023?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
           />
-          <h2 className="absolute text-5xl text-yellow-300 font-bold">
+          <h2 className="absolute md:text-5xl text-2xl text-yellow-300 font-bold">
             May our city live green.
           </h2>
         </SwiperSlide>
@@ -39,7 +39,7 @@ export default function Slider() {
             src="https://plus.unsplash.com/premium_photo-1723114876196-3ad72b4d17f6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGJsb29kJTIwZG9uYXRpb258ZW58MHx8MHx8fDA%3D"
             alt=""
           />
-          <h2 className="absolute text-5xl text-blue-600 font-bold">
+          <h2 className="absolute md:text-5xl text-2xl text-blue-600 font-bold">
             One bag of blood, one new life
           </h2>
         </SwiperSlide>
@@ -48,7 +48,7 @@ export default function Slider() {
             src="https://plus.unsplash.com/premium_photo-1661964155049-f8a24a60be74?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZHJlc3MlMjBkb25hdGlvbnxlbnwwfHwwfHx8MA%3D%3D"
             alt=""
           />
-          <h2 className="absolute text-5xl text-blue-600 font-bold">
+          <h2 className="absolute md:text-5xl text-2xl text-blue-600 font-bold">
             Help distribute winter clothes/clothes
           </h2>
         </SwiperSlide>
@@ -57,7 +57,7 @@ export default function Slider() {
             src="https://images.unsplash.com/photo-1758599668429-121d54188b9c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332"
             alt=""
           />
-          <h2 className="absolute text-5xl text-white font-bold">
+          <h2 className="absolute md:text-5xl text-2xl text-white font-bold">
             We keep it clean wherever We live.
           </h2>
         </SwiperSlide>
